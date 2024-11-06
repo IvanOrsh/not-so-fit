bebcaec318a6b772891bf79d6948ba02440bb30d
IvanOrsh <ksironov@mail.ru>
2024-11-06 23:45:02 +0300

    feat: scaffold hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Hero from './Hero.svelte';
+</script>
+
+<Hero />
+
+<a href="/programme">This week&apos;s programme</a>
+<a href="/personal">Generate a programme</a>
+<a href="/discord">Join the community</a>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,21 @@
+<div class="stack">
+	<div class="background"></div>
+	<h1>Not So Fit</h1>
+</div>
+
+<style lang="postcss">
+	.stack {
+		background: linear-gradient(90deg in oklab, orange, teal);
+		height: 50svh;
+	}
+	.background {
+		margin: 1rem;
+		border-radius: 1rem;
+		background: linear-gradient(90deg in oklab, #1c1917, #292524);
+	}
+	h1 {
+		color: papayawhip;
+		font-size: 3rem;
+		place-self: center;
+	}
+</style>
