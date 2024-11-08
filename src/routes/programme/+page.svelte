40177eddc0742725eac871e28748bd126791cf87
IvanOrsh <ksironov@mail.ru>
2024-11-08 23:56:11 +0300

    feat: scaffold programme route

---
+++ src/routes/programme/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Anchor from '$lib/components/Anchor.svelte';
+</script>
+
+<h1 class="text-6xl">Programme</h1>
+
+<p>Week starting 8/11/25</p>
+
+<ul>
+	<li>
+		<h2>Lower 1</h2>
+		<p>Core Exercise</p>
+	</li>
+	<li>
+		<h2>Upper 1</h2>
+		<p>Core Exercise</p>
+	</li>
+	<li>
+		<h2>Bro 1</h2>
+		<p>Core Exercise</p>
+	</li>
+	<li>
+		<h2>Lower 2</h2>
+		<p>Core Exercise</p>
+	</li>
+	<li>
+		<h2>Upper 2</h2>
+		<p>Core Exercise</p>
+	</li>
+</ul>
+
+<button>Download</button>
